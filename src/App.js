b058ad7ff72195426351890c50214f334a37cf17
Josiah <josiah617owl@gmail.com>
2022-05-26 21:26:25 -0700

    Editing

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          THIS IS MINE NOW
+          THIS IS MINE NOW?
         </p>
         <a
           className="App-link"
